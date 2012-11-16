320938edf1464dbdc96ae6804490433a57b000af
kostafey <kostafey@gmail.com>
2012-11-16 16:25:57 +0400

    Current OS verification before set x-cut-buffer-or-selection-value.

--- .emacs.d/custom/key-bindings.el
+++ .emacs.d/custom/key-bindings.el
@@ -16,8 +16,10 @@
 ;;
 ;; Region selection:
 (setq transient-mark-mode t)
-(setq x-select-enable-clipboard t)
-(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+
+(when (eq system-type 'gnu/linux)
+  (setq x-select-enable-clipboard t)
+  (setq interprogram-paste-function 'x-cut-buffer-or-selection-value))
 ;;-----------------------------------------------------------------------------
 (global-set-key (kbd "C-S-v") 'cua-paste-pop)
 (global-set-key (kbd "M-S-v") '(lambda() (interactive) (cua-paste-pop -1)))
