31c3ffe83033d618567ac047f48b429502f2a6bb
kostafey <kostafey@gmail.com>
2013-04-10 22:02:35 +0400

    Shell script generation for clojure-offline is added.

--- .emacs.d/solutions/clojure-offline.el
+++ .emacs.d/solutions/clojure-offline.el
@@ -1,4 +1,4 @@
-;;; clojure-offline.el -- The collection of functions to help use clojure offline.
+;;; clojure-offline.el -- The set of functions to help use clojure offline.
 
 ;;; Copyright © 2013 - Kostafey <kostafey@gmail.com>
 
@@ -16,11 +16,37 @@
 ;;; along with this program; if not, write to the Free Software Foundation,
 ;;; Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.  */
 
+;;; Usage:
+
+;; 1. Select (mark) dependencies vector in the defproject expression of the 
+;;    project.clj file, like here (selection is marked by # symbols):
+;;
+;;   :dependencies #[[org.clojure/clojure "1.5.1"]
+;;                   [compojure "1.1.5"]
+;;                   [me.raynes/laser "1.1.1"]
+;;                   [mysql/mysql-connector-java "5.1.24"]
+;;                   [korma "0.3.0-RC5"]
+;;                   [lib-noir "0.4.9"]]#
+;;
+;; 2. Run M-x clj-off-create-script RET RET
+
+(defvar clojure-offline-script-buffer-name "*clojure-offline*")
+
 (defun force-symbol-name (some-symbol)
   "Return lisp symbol `some-symbol' as a string at all costs!"
-  (mapconcat 'char-to-string 
+  (mapconcat 'char-to-string
              (string-to-list (symbol-name some-symbol)) ""))
 
+(defun trim-string (string)
+  "Remove white spaces in beginning and ending of STRING.
+White space here is any of: space, tab, emacs newline (line feed, ASCII 10)."
+  (replace-regexp-in-string 
+   "\\`[ \t\n]*" "" 
+   (replace-regexp-in-string "[ \t\n]*\\'" "" string)))
+
+;;----------------------------------------------------------------------
+;;
+;;;###autoload
 (defun clj-off-parse-artifact (artifact-name)
   "Parse `artifact-name' to list (`group-id' `artifact-id' `version')
 Input formats, e.g.:
@@ -28,38 +54,49 @@ Input formats, e.g.:
  org.clojure:clojure:pom:1.5.1
 Ouptut format, e.g.:
  (\"org.clojure\" \"clojure\" \"1.5.1\")"
-  (let* ((is-lein-format (vectorp artifact-name))
-         artifact-list
-         group-and-artifact
-         group-id
-         artifact-id
-         version)
-    (if is-lein-format
-        ;; lein format, e.g. [org.clojure/clojure "1.5.1"]
+  ;; lein format as string e.g. "[org.clojure/clojure \"1.5.1\"]"
+  (let* ((artifact-name
+          (if (and (stringp artifact-name)
+                   (equal (substring (trim-string artifact-name) 0 1) "["))
+              (read (trim-string artifact-name))
+            artifact-name)))
+    (let* ((is-lein-format (vectorp artifact-name))
+           artifact-list
+           group-and-artifact
+           group-id
+           artifact-id
+           version)
+      (if is-lein-format
+          ;; lein format, e.g. [org.clojure/clojure "1.5.1"]
+          (progn
+            (setq group-and-artifact
+                  (split-string (force-symbol-name (elt artifact-name 0)) "/"))
+            (setq group-id (nth 0 group-and-artifact))
+            (setq artifact-id (if (> (length group-and-artifact) 1)
+                                  (nth 1 group-and-artifact)
+                                (nth 0 group-and-artifact)))
+            (setq version (elt artifact-name 1)))
+        ;; maven format, e.g. "org.clojure:clojure:pom:1.5.1"
         (progn
-          (setq group-and-artifact
-                (split-string (force-symbol-name (elt artifact-name 0)) "/"))
-          (setq group-id (nth 0 group-and-artifact))
-          (setq artifact-id (if (> (length group-and-artifact) 1)
-                             (nth 1 group-and-artifact)
-                           (nth 0 group-and-artifact)))
-          (setq version (elt artifact-name 1)))
-      ;; maven format, e.g. "org.clojure:clojure:pom:1.5.1"
-      (progn
-        (setq artifact-list (split-string artifact-name ":"))
-        (setq group-id (nth 0 artifact-list))
-        (setq artifact-id (nth 1 artifact-list))
-        (setq version (nth 3 artifact-list))))
-    (list group-id artifact-id version)))
+          (setq artifact-list (split-string artifact-name ":"))
+          (setq group-id (nth 0 artifact-list))
+          (setq artifact-id (nth 1 artifact-list))
+          (setq version (nth 3 artifact-list))))
+      (list group-id artifact-id version))))
 
+;;;###autoload
 (defmacro clj-off-with-artifact (artifact-name &rest body)
-  "Inject `group-id' `artifact-id' `version' local variables to the `body' scope."
+  "Inject `group-id' `artifact-id' `version' local variables to the `body'
+scope."
   `(let* ((artifact (clj-off-parse-artifact ,artifact-name))
           (group-id (nth 0 artifact))
           (artifact-id (nth 1 artifact))
           (version (nth 2 artifact)))
      ,@body))
 
+;;----------------------------------------------------------------------
+;;
+;;;###autoload
 (defun clj-off-get-clojars-url (artifact-name)
   "Convert from maven's `artifact-name' to probably jar url location on
 clojars.
@@ -74,6 +111,21 @@ https://clojars.org/repo/lein-ring/lein-ring/0.8.2/lein-ring-0.8.2.jar"
            artifact-id "/" version "/"
            artifact-id "-" version ".jar")))
 
+(defun clj-off-get-list-clojars-url (artifact-names-array)
+  (map 'list 'clj-off-get-clojars-url artifact-names-array))
+
+;;;###autoload
+(defun clj-off-type-clojars-url (artifact-name)
+  (interactive
+   (list
+    (read-from-minibuffer "Clojure artifact: "
+                          (buffer-substring (mark) (point)) nil nil
+                          'clj-off-artifact-name-history)))
+  (message (clj-off-get-clojars-url artifact-name)))
+
+;;----------------------------------------------------------------------
+;;
+;;;###autoload
 (defun clj-off-get-mvn-deploy (artifact-name)
   "Convert from maven's `artifact-name' to local maven repository creation
 script.
@@ -93,5 +145,33 @@ mvn deploy:deploy-file -DgroupId=lein-ring -DartifactId=lein-ring \
            "-Dfile=" artifact-id "-" version ".jar" " "
            "-Durl=" "file:maven_repository")))
 
-(provide 'clojure-offline)
+(defun clj-off-get-list-mvn-deploy (artifact-names-array)
+  (map 'list 'clj-off-get-mvn-deploy artifact-names-array))
 
+;;----------------------------------------------------------------------
+;;
+(defun clj-off-create-script-buffer ()
+  "Create buffer dedicated to output configure required clojure jars."
+  (get-buffer-create clojure-offline-script-buffer-name))
+
+;;;###autoload
+(defun clj-off-create-script (artifact-names-array)
+  (interactive
+   (list
+    (read-from-minibuffer "Clojure artifacts list: "
+                          (buffer-substring (mark) (point)) nil nil
+                          'clj-off-artifacts-list-history)))
+  (let ((artifact-names-array (if (stringp artifact-names-array)
+                                  (read (trim-string artifact-names-array))
+                                artifact-names-array)))
+    (set-buffer (clj-off-create-script-buffer))
+    (erase-buffer)
+    (toggle-truncate-lines nil)
+    (mapc (lambda (art) (insert (concat "wget " art "\n")))
+          (clj-off-get-list-clojars-url artifact-names-array))
+    (insert "\n")
+    (mapc (lambda (art) (insert (concat art "\n")))
+          (clj-off-get-list-mvn-deploy artifact-names-array))
+    (switch-to-buffer (clj-off-create-script-buffer))))
+
+(provide 'clojure-offline)
