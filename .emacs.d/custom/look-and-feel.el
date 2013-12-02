27f09e64dab102ac40510e92c2a9a220230c097f
kostafey <kostafey@gmail.com>
2013-12-03 00:28:48 +0400

    Add maximize window on Linux.

--- .emacs.d/custom/look-and-feel.el
+++ .emacs.d/custom/look-and-feel.el
@@ -127,6 +127,13 @@
          (progn (toggle-fullscreen) 
                 (previous-buffer))))
 
+(defun fullscreen-linux (&optional f)
+       (interactive)
+       (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+	    		 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+       (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+	    		 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
+
 (defun toggle-full-screen-win32 ()
  "Toggles full-screen mode for Emacs window on Win32."
  (interactive)
