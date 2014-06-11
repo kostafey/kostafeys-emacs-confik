0828fb70be06c96698692e8c6b575454b3e02d50
kostafey <kostafey@gmail.com>
2014-06-11 15:06:12 +0400

    Use magit-status by default for version control.

--- custom/version-control.el
+++ custom/version-control.el
@@ -8,18 +8,19 @@
 (defun get-vc-status ()
   "Call status function according to the actual vc system of the active file."
   (interactive)
-  (let ((vc-type (downcase 
-                  (format "%s" 
-                          (vc-backend 
+  (let ((vc-type (downcase
+                  (format "%s"
+                          (vc-backend
                            (buffer-file-name (current-buffer)))))))
     (cond ((equal vc-type "hg")  (ahg-status))
-          ((equal vc-type "git") (magit-status 
+          ((equal vc-type "git") (magit-status
                                   (if current-prefix-arg
                                       (magit-read-top-dir
-                                       (> (prefix-numeric-value 
+                                       (> (prefix-numeric-value
                                            current-prefix-arg)
                                           4))
                                     (or (magit-get-top-dir)
-                                        (magit-read-top-dir nil))))))))
+                                        (magit-read-top-dir nil)))))
+          (t (magit-status nil)))))
 
 (provide 'version-control)
