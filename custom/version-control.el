ee699d4168cbdbd90dd203381b490265e91e093d
kostafey <kostafey@gmail.com>
2015-07-05 13:44:49 +0400

    Update magit-status usage.

--- custom/version-control.el
+++ custom/version-control.el
@@ -16,14 +16,7 @@
                           (vc-backend
                            (buffer-file-name (current-buffer)))))))
     (cond ((equal vc-type "hg")  (ahg-status))
-          ((equal vc-type "git") (magit-status
-                                  (if current-prefix-arg
-                                      (magit-read-top-dir
-                                       (> (prefix-numeric-value
-                                           current-prefix-arg)
-                                          4))
-                                    (or (magit-get-top-dir)
-                                        (magit-read-top-dir nil)))))
-          (t (magit-status nil)))))
+          ((equal vc-type "git") (magit-status))
+          (t (magit-status)))))
 
 (provide 'version-control)
