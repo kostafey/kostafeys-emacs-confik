dae7ab828a4abef0fea3d8ff1398c973abb911f4
kostafey <kostafey@gmail.com>
2015-09-10 22:57:30 +0400

    Add melpa-stable repository usage.

--- custom/elpa-conf.el
+++ custom/elpa-conf.el
@@ -10,6 +10,7 @@
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("m-stable" . "http://stable.melpa.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
 
