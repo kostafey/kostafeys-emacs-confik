aa32b1c72da4c2bc49503c77427a4efc875ffe27
kostafey <kostafey@gmail.com>
2015-09-29 15:08:48 +0400

    Add what-face function.

--- custom/look-and-feel.el
+++ custom/look-and-feel.el
@@ -59,6 +59,12 @@
 (load-theme 'organic-green t)
 ;;-----------------------------------------------------------------------------
 
+(defun what-face (pos)
+  (interactive "d")
+  (let ((face (or (get-char-property (point) 'read-face-name)
+                  (get-char-property (point) 'face))))
+    (if face (message "Face: %s" face) (message "No face at %d" pos))))
+
 ;;=============================================================================
 ;; StatusBar config
 ;;
