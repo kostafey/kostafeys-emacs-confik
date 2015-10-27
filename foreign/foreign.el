2edc1f498150a4f58117821d8c426fc7820783a4
kostafey <kostafey@gmail.com>
2015-10-27 17:54:14 +0400

    Adjust foreign.el.

--- foreign/foreign.el
+++ foreign/foreign.el
@@ -1,32 +1,75 @@
-;; External programs wrapper
+;;; foreign.el -- External text-manipulation programs wrapper
 
-(defvar foreign-bin-path (expand-file-name "foreign" "~/.emacs.d"))
+;; Author: Kostafey <kostafey@gmail.com>
+;; Keywords: text-manipulation, performance
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software Foundation,
+;; Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.  */
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; See docstrings for the following functions:
+
+;; * foreign-format-json
+;; * foreign-replace
+
+(require 'functions)
+
+(defvar foreign-bin-path (concat-path "~/.emacs.d" "foreign"))
+
+(defun foreign-get-go-executable (src-go-file)
+  (let* ((src-go-path (concat-path foreign-bin-path src-go-file))
+         (bin-go-path (substring src-go-path 0 (- (length src-go-path) 3))))
+    (if (not (file-exists-p bin-go-path))
+        (shell-command
+         (concat "bash - c \"go build -o " bin-go-path " " src-go-path"\"")))
+    bin-go-path))
 
 (defun foreign-format-json-file (file-name)
-  (shell-command
-   (concat foreign-bin-path "jsonpp " file-name)))
+  (let ((bin (foreign-get-go-executable "jsonpp.go")))
+    (shell-command
+     (concat bin " " file-name))))
 
 (defun foreign-replace-file (file-name old-string new-string)
-  (shell-command
-   (concat foreign-bin-path "replace " file-name
-           " " old-string " " new-string)))
+  (let ((bin (foreign-get-go-executable "replace.go")))
+    (shell-command
+     (concat bin " " file-name " " old-string " " new-string))))
 
-(defun json-file-format ()
+(defun foreign-format-json ()
   "Pretty print json file."
   (interactive)
   (js-mode)
   (foreign-format-json-file (buffer-file-name))
-  (revert-buffer t t))
+  (revert-buffer t t)
+  (message "Done."))
 
-(defun replace-file (old-string new-string)
+(defun foreign-replace (old-string new-string)
   "Replace string in file."
   (interactive
    (list
     (read-from-minibuffer "Replace string: "
-                          "" nil nil 'replace-file-old-history)
-    (read-from-minibuffer (concat "Replace with:")
-                          "" nil nil 'replace-file-new-history)))
-  (foreign-replace-file (buffer-file-name) old-string new-string)
-  (revert-buffer t t))
+                          "" nil nil 'foreign-replace-old-history)
+    (read-from-minibuffer (concat "Replace with: ")
+                          "" nil nil 'foreign-replace-new-history)))
+  (foreign-replace-file (buffer-file-name)
+                        (add-quotes (replace-newlines old-string))
+                        (add-quotes (replace-newlines new-string)))
+  (let ((file-name (buffer-file-name)))
+    (kill-buffer (current-buffer))
+    (find-file file-name)
+    (message "Done.")))
 
 (provide 'foreign)
--- solutions/functions.el
+++ solutions/functions.el
@@ -1,6 +1,6 @@
 ;;; functions.el -- The collection of misc elisp helper functions.
 
-;;; Copyright © 2013-2014 - Kostafey <kostafey@gmail.com>
+;;; Copyright © 2013-2015 - Kostafey <kostafey@gmail.com>
 
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -38,73 +38,9 @@ E.g.
   (let ((path))
     (dolist (folder folders)
       (setq path (if (equal ".." folder)
-                     (file-name-as-directory (expand-file-name ".." path))
-                   (file-name-as-directory (concat path folder)))))
+                     (expand-file-name ".." path)
+                   (expand-file-name folder path))))
     path))
-
-(defun find-file-recursively (search-file-name
-                              &optional start-path fail-on-error)
-  "Return the full path to SEARCH-FILE-NAME.
-Recursive searching starts from START-PATH.
-When FAIL-ON-ERROR is t, raise error if nothing found, return nil otherwise."
-  (let ((start-path (if start-path start-path "/"))
-        (result nil))
-    (if search-file-name
-        (dolist (path-entity (directory-files-and-attributes start-path t))
-          (if (not result)
-              (let ((entity-name (car path-entity)))
-                (if (and
-                     (cadr path-entity) ; is a directory sign
-                     (not (or (equal (file-name-nondirectory entity-name) ".")
-                              (equal (file-name-nondirectory entity-name) "..")
-                              (equal (substring
-                                      (file-name-nondirectory entity-name)
-                                      0 2) ".#"))))
-                    ;; is a directory
-                    (setq result (find-file-recursively
-                                  search-file-name
-                                  entity-name nil))
-                  ;; is a file
-                  (if (equal (downcase search-file-name)
-                             (downcase (file-name-nondirectory entity-name)))
-                      (setq result entity-name)))))))
-    (if (and fail-on-error (not result))
-        (error (concat "Can't find file " search-file-name)))
-    result))
-
-(defun find-file-upwards (search-file-name start-path
-                          &optional ascent-depth fail-on-error)
-  "Return the full path to `search-file-name' by moving upwards
-on the directory tree.
-START-PATH is path where searching starts.
-ASCENT-DEPTH is a maximim number of '..' acts on the directory tree.
-When FAIL-ON-ERROR is t, raise error if nothing found,return nil otherwise."
-  (let ((ascent-depth (if ascent-depth ascent-depth 0))
-        (result nil))
-    (if (and start-path search-file-name)
-        (dolist (path-entity (directory-files-and-attributes start-path t))
-          (if (not result)
-              (let ((entity-name (car path-entity)))
-                (if (not
-                     (and
-                      (cadr path-entity) ; is a directory sign
-                      (not (or (equal (file-name-nondirectory entity-name) ".")
-                               (equal (file-name-nondirectory entity-name) "..")
-                               (equal (substring
-                                       (file-name-nondirectory entity-name)
-                                       0 2) ".#")))))
-                  ;; is a file
-                  (if (equal (downcase search-file-name)
-                             (downcase (file-name-nondirectory entity-name)))
-                      (setq result entity-name)))))))
-    (if (and (not result) (> ascent-depth 0))
-        (setq result (find-file-upwards search-file-name
-                                        (clomacs-concat-path start-path "..")
-                                        (1- ascent-depth)
-                                        fail-on-error)))
-    (if (and fail-on-error (not result))
-        (error (concat "Can't find file " search-file-name)))
-    result))
 
 (defun force-symbol-name (some-symbol)
   "Return lisp symbol `some-symbol' as a string at all costs!"
@@ -112,6 +48,7 @@ When FAIL-ON-ERROR is t, raise error if nothing found,return nil otherwise."
              (string-to-list (symbol-name some-symbol)) ""))
 
 (defun strip-text-properties(txt)
+  "Remove text properties from the string."
   (set-text-properties 0 (length txt) nil txt)
       txt)
 
@@ -121,6 +58,14 @@ White space here is any of: space, tab, emacs newline (line feed, ASCII 10)."
   (replace-regexp-in-string
    "\\`[ \t\n]*" ""
    (replace-regexp-in-string "[ \t\n]*\\'" "" string)))
+
+(defun add-quotes (str)
+  (concat "\"" str "\""))
+
+(defun replace-newlines (txt)
+  "Replace (kbd c-q c-j) with \n"
+  (replace-regexp-in-string "
+" "\\\\n" txt))
 
 (defun kostafey-export-keys ()
   "Export my Emacs keybindings configuration."
