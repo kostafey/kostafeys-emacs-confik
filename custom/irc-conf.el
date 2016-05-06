134fcf668650af8eb08768e9769b2a58c2b3f7dc
kostafey <kostafey@gmail.com>
2016-05-06 19:18:58 +0300

    Enable circe-color-nicks-everywhere.

--- custom/irc-conf.el
+++ custom/irc-conf.el
@@ -1,6 +1,7 @@
 (eval-after-load 'circe
   '(progn
      (setq circe-reduce-lurker-spam t)
-     (enable-circe-color-nicks)))
+     (enable-circe-color-nicks)
+     (setq circe-color-nicks-everywhere t)))
 
 (provide 'irc-conf)
