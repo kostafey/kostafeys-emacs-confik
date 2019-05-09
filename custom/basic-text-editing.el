ee5d4f0b448a16fb5ff616675ce9beb61addddc4
kostafey <kostafey@gmail.com>
2019-05-09 17:23:30 +0300

    Add insert-random-uuid fn.

--- custom/basic-text-editing.el
+++ custom/basic-text-editing.el
@@ -503,6 +503,40 @@ Version 2015-09-14."
     (delete-region -p1 -p2)
     (insert (decode-coding-string (url-unhex-string -input-str) 'utf-8))))
 
+;;;###autoload
+(defun insert-random-uuid ()
+  "Insert a UUID. This uses a simple hashing of variable data.
+Example of a UUID: 1df63142-a513-c850-31a3-535fc3520c3d
+
+Note: this code uses https://en.wikipedia.org/wiki/Md5 , which is not
+cryptographically safe. I'm not sure what's the implication of its use here.
+
+Version 2015-01-30
+URL `http://ergoemacs.org/emacs/elisp_generate_uuid.html'
+"
+  ;; by Christopher Wellons, 2011-11-18. Editted by Xah Lee.
+  ;; Edited by Hideki Saito further to generate all valid variants for "N" in
+  ;; xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx format.
+  (interactive)
+  (let ((myStr (md5 (format "%s%s%s%s%s%s%s%s%s%s"
+                            (user-uid)
+                            (emacs-pid)
+                            (system-name)
+                            (user-full-name)
+                            (current-time)
+                            (emacs-uptime)
+                            (garbage-collect)
+                            (buffer-string)
+                            (random)
+                            (recent-keys)))))
+    (insert (format "%s-%s-4%s-%s%s-%s"
+                    (substring myStr 0 8)
+                    (substring myStr 8 12)
+                    (substring myStr 13 16)
+                    (format "%x" (+ 8 (random 4)))
+                    (substring myStr 17 20)
+                    (substring myStr 20 32)))))
+
 (defun spaces-to-tabs ()
   "Reaplace 4 spaces with a tab char."
   (interactive)
