ba71606c3b7566d7a5e5a000b466b1be50c1cb99
kostafey <kostafey@gmail.com>
2019-09-17 15:42:11 +0300

    Update mode-line-conf.el

--- custom/mode-line-conf.el
+++ custom/mode-line-conf.el
@@ -1,78 +1,87 @@
 ;;=============================================================================
 ;; mode-line (emacs status bar) config
 ;;
-(column-number-mode t)
-(setq display-time-format "%H:%M")
-(display-time-mode t)
-
-(when (require 'battery nil 'noerror)
-  (progn
-    (setq battery−mode−line−format " [%L %p%% %dC]")
-    (when (and battery-status-function
-       (not (string-match-p "N/A" 
-                (battery-format "%B"
-                        (funcall battery-status-function)))))
-      (display-battery-mode 1))))
-
-(setq-default mode-line-format 
-  (list ""
-        ;; file encoding
-        'mode-line-mule-info
-        '(:eval (propertize
-                 (format"%s " buffer-file-coding-system)
-                 ;; 'face 'mode-line-default-face                 
-                 'help-echo (format"%s" buffer-file-coding-system)))        
-                
-        ;; the buffer name; the file name as a tool tip
-        '(:eval (propertize (format-mode-line mode-line-buffer-identification)
-                            ;; 'face 'mode-line-header
-                            'help-echo (buffer-file-name)))
-
-        ;; line and column
-        " (" ;; '%02' to set to 2 chars at least; prevents flickering
-        (propertize "%02l" 'face 'font-lock-string-face) ","
-        (propertize "%02c" 'face 'font-lock-string-face) 
-        ") "
-
-        ;; relative position, size of file
-        "("
-        (propertize "%p" 'face 'font-lock-constant-face) ;; % above top
-        "/"
-        (propertize "%I" 'face 'font-lock-constant-face) ;; size
-        ")"
-        
-        '(vc-mode vc-mode)
-        ;; 'mode-line-process
-        " "
-        ;; the current major mode for the buffer and list of minor modes.
-        'mode-line-modes       
-
-        "(" ;; insert vs overwrite mode, input-method in a tooltip
-        '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
-                            'face 'font-lock-preprocessor-face
-                            'help-echo (concat "Buffer is in "
-                                               (if overwrite-mode "overwrite" "insert") " mode")))
-
-        ;; was this buffer modified since the last save?
-        '(:eval (when (buffer-modified-p)
-                  (concat ","  (propertize "*"
-                                           'face 'font-lock-string-face
-                                           'help-echo "Buffer has been modified"))))
-
-        ;; is this buffer read-only?
-        '(:eval (when buffer-read-only
-                  (concat ","  (propertize "RO"
-                                           'face 'font-lock-string-face
-                                           'help-echo "Buffer is read-only"))))  
-        ") "
-
-        ;; add the time, with the date and the emacs uptime in the tooltip
-        ;; and system load average
-        '(:eval (propertize (format-mode-line global-mode-string)
-                            ;; 'help-echo (concat (format-time-string "%c; ")
-                            ;;                    (emacs-uptime "Uptime:%hh"))
-                            ))
-        ))
+(setq-default
+ projectile-mode-line-prefix " "
+ mode-line-format
+ (list ""
+       ;; ------------------------------------------------------------
+       ;; file encoding
+       'mode-line-mule-info
+       '(:eval (propertize
+                (format "%s " buffer-file-coding-system)
+                'help-echo (format"%s" buffer-file-coding-system)))        
+       ;; ------------------------------------------------------------
+       ;; the buffer name; the file name as a tool tip
+       '(:eval (propertize (buffer-name)
+                           'face '(:weight bold)
+                           'mouse-face 'mode-line-highlight
+                           'help-echo (buffer-file-name)
+                           'local-map (let ((map (make-sparse-keymap)))
+                                        (define-key map [mode-line mouse-1]
+                                          (lambda (e)
+                                            (interactive "e")
+                                            (mode-line-previous-buffer e)))
+                                        (define-key map [mode-line mouse-3]
+                                          (lambda (e)
+                                            (interactive "e")
+                                            (mode-line-next-buffer e)))
+                                        map)))
+       ;; ------------------------------------------------------------
+       ;; line and column
+       " (" ;; '%02' to set to 2 chars at least; prevents flickering
+       (propertize "%02l" 'face 'font-lock-string-face) ","
+       (propertize "%02c" 'face 'font-lock-string-face) 
+       ")"        
+       ;; ------------------------------------------------------------
+       ;; the current major mode for the buffer and list of minor modes.
+       '(:eval (propertize " %m"
+                           'face '((t :foreground "black"))
+                           'help-echo (mapconcat
+                                       'identity
+                                       (-sort
+                                        'string-lessp
+                                        (-map 'symbol-name
+                                              (--filter
+                                               (and (boundp it)
+                                                    (symbol-value it))
+                                               minor-mode-list)))
+                                       "\n")
+                           'mouse-face 'mode-line-highlight
+                           'local-map (make-mode-line-mouse-map
+                                       'mouse-1 'describe-mode)))
+       ;; ------------------------------------------------------------
+       ;; version control data
+       '(:eval (propertize (progn
+                             (vc-mode-line buffer-file-name)
+                             vc-mode)
+                           'face 'font-lock-constant-face))
+       ;; ------------------------------------------------------------
+       ;; projectile
+       '(:eval (propertize (projectile-default-mode-line)
+                           'mouse-face 'mode-line-highlight
+                           'local-map (make-mode-line-mouse-map
+                                       'mouse-1 'projectile-mode-menu)))
+       ;; ------------------------------------------------------------
+       ;; read only, insert/overwrite, edited signs
+       " (" ;; insert vs overwrite mode, input-method in a tooltip
+       '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
+                           'face 'font-lock-preprocessor-face
+                           'help-echo (concat "Buffer is in "
+                                              (if overwrite-mode
+                                                  "overwrite"
+                                                "insert") " mode")))
+       ;; was this buffer modified since the last save?
+       '(:eval (when (buffer-modified-p)
+                 (concat ","  (propertize "*"
+                                          'face 'font-lock-string-face
+                                          'help-echo "Buffer has been modified"))))
+       ;; is this buffer read-only?
+       '(:eval (when buffer-read-only
+                 (concat ","  (propertize "RO"
+                                          'face 'font-lock-string-face
+                                          'help-echo "Buffer is read-only"))))  
+       ")"                
+       ))
 
 (provide 'mode-line-conf)
-
