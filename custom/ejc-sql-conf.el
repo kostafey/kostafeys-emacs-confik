23b69709d673f26b4eec59c852472d44b25a2d50
kostafey@gmail.com <kostafey@gmail.com>
2020-07-12 15:28:51 +0300

    Set ejc-org-mode-show-results to nil

--- custom/ejc-sql-conf.el
+++ custom/ejc-sql-conf.el
@@ -6,6 +6,9 @@
 (setq nrepl-sync-request-timeout 60)
 (setq clomacs-httpd-default-port 8090) ; Use a port other than 8080.
 
+;; Show results of SQL snippets evaluation in `org-mode'
+;; in dedicated buffer.
+(setq ejc-org-mode-show-results nil)
 (setq ejc-use-flx t)                          ; Enable `flx' fuzzy matching.
 (setq ejc-result-table-impl 'ejc-result-mode) ; Set major-mode for results.
 ;; (setq ejc-result-table-impl 'orgtbl-mode)  ; Default major-mode for results.
