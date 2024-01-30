1fe3b87ced2d8d019ab1a9f7905808ef51f2a597
kostafey <kostafey@gmail.com>
2024-01-30 04:25:30 +0300

    Set google-translate-translation-to-kill-ring true

--- custom/translate-conf.el
+++ custom/translate-conf.el
@@ -5,5 +5,7 @@
 (require 'google-translate-smooth-ui)
 (global-set-key (kbd "C-C t") 'google-translate-smooth-translate)
 (global-set-key (kbd "C-c t") 'google-translate-at-point)
+;; Add translation to kill-ring after translate commands if it's `t'.
+(setq google-translate-translation-to-kill-ring t)
 
 (provide 'translate-conf)
