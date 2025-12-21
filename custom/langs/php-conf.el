17ebbfb3d90eb6d064f65f60896f689493b0ff1d
kostafey <kostafey@gmail.com>
2025-12-22 01:00:11 +0300

    Require php-mode package

--- custom/langs/php-conf.el
+++ custom/langs/php-conf.el
@@ -1,5 +1,9 @@
 ;;; php-conf.el --- PHP configuration for lsp
 
+(straight-use-package
+ '(php-mode :type git :host github
+				    :repo "emacs-php/php-mode" :branch "master"))
+
 (add-hook 'php-mode-hook 'lsp)
 
 (provide 'php-conf)
