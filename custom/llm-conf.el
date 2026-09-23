af8e4673c7c5fa20e94ce365f1aa664b736fdd42
kostafey <kostafey@gmail.com>
2026-09-23 13:41:26 +0200

    Update local LLMs launch examples list

--- custom/llm-conf.el
+++ custom/llm-conf.el
@@ -3,13 +3,22 @@
 ;; Start server:
 ;; -------------
 ;; `Qwen2.5-Coder-3B'
-;; llama serve -hf bartowski/Qwen2.5-Coder-3B-Instruct-GGUF:Q5_K_M --port 8012 --cors-origins "http://localhost:8012"
+;; llama serve -hf bartowski/Qwen2.5-Coder-3B-Instruct-GGUF:Q5_K_M --port 8012
+;;
+;; `Qwen2.5-3B'
+;; llama serve -hf bartowski/Qwen2.5-3B-Instruct-GGUF:Q5_K_M --port 8012
 ;;
 ;; `Qwen3-8B'
 ;; reasoning-budget 128:
-;; llama serve -hf Qwen/Qwen3-8B-GGUF:Q4_K_M --reasoning-format deepseek --reasoning-budget 128 --reasoning-budget-message "Enough thinking, give the final answer now." -c 16384 -ngl 99 --port 8012 --cors-origins "http://localhost:8012"
-;; thinking off:
-;; llama serve -hf Qwen/Qwen3-8B-GGUF:Q4_K_M --reasoning off -c 16384 -ngl 99 --port 8012 --cors-origins "http://localhost:8012"
+;; llama serve -hf Qwen/Qwen3-8B-GGUF:Q4_K_M --reasoning-format deepseek --reasoning-budget 128 --reasoning-budget-message "Enough thinking, give the final answer now." -c 16384 -ngl 99 --port 8012
+;; reasoning off:
+;; llama serve -hf Qwen/Qwen3-8B-GGUF:Q4_K_M --reasoning off -c 16384 -ngl 99 --port 8012
+;;
+;; `Qwen3-4B'
+;; llama serve -hf Qwen/Qwen3-4B-GGUF:Q4_K_M --reasoning off -c 4096 -ngl 99 --device Vulkan1 -fa on -ctk q8_0 -ctv q8_0 --port 8012
+;;
+;; `Gemma-3-4B'
+;; llama serve -hf unsloth/gemma-3-4b-it-GGUF:Q4_K_M -c 8192 -ngl 99 --device Vulkan1 -fa on -ctk q8_0 -ctv q8_0 --port 8012
 ;;
 ;; Qwen3 thinks by default. `--reasoning-format deepseek' keeps the thoughts
 ;; out of `message.content' and `gptel-include-reasoning' set to nil drops
